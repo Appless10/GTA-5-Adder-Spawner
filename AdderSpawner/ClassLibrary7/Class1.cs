94de51c7621a6c996e1050ec15b02f0e51ee8e2c
Appless10 <94022547+Appless10@users.noreply.github.com>
2022-12-04 18:21:56 -0500

    Add files via upload

---
+++ AdderSpawner/ClassLibrary7/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+
+namespace TutorialScript
+{
+
+
+    public class Main : Script
+    {
+        Vehicle car;
+        int GameTimeReference = Game.GameTime + 1000;
+        public Main()
+        {
+            Tick += onTick;
+            KeyDown += onKeyDown;
+            GameTimeReference = Game.GameTime + 1000;
+        }
+
+        private void onTick(object sender, EventArgs e)
+        {
+
+            if (Game.GameTime > GameTimeReference)
+            {
+                if (Function.Call<bool>(Hash.IS_PED_IN_ANY_VEHICLE, Game.Player.Character, false)) ;
+                {
+                    car.Repair();
+                    GameTimeReference = Game.GameTime + 1000;
+                }
+            }
+
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.NumPad7)
+            {
+                car = World.CreateVehicle(VehicleHash.Adder, Game.Player.Character.GetOffsetInWorldCoords(new Vector3(0, 5, 0)));
+                UI.Notify("Adder has been ~b~spawned!");
+            }
+        }
+
+    }
+}
